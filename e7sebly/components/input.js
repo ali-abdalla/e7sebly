7fbdec024b40983329c24174a74778641e497856
Abdo <abdo38013@gmail.com>
2018-06-22 15:09:10 +0200

    add calculate functionality

--- e7sebly/components/input.js
+++ e7sebly/components/input.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { Text, View,TextInput,StyleSheet } from 'react-native';
 
 export default class Input extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      value:this.props.value
+    };
+  }
+  onChange=(text)=>{
+    this.setState({value:text});
+    this.props.updateStatus(text);
+  }
   render() {
     return (
       <View>
         <Text>{this.props.label}</Text>
-        <TextInput style={styles.textInput}></TextInput>
+        <TextInput keyboardType={this.props.type} onChangeText={this.onChange} style={styles.textInput} value={this.state.value}></TextInput>
       </View>
     );
   } 
--- e7sebly/order.js
+++ e7sebly/order.js
@@ -3,21 +3,45 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Input from "./components/input";
 
 export default class Order extends React.Component {
-    onPressCalculate() {
-
+    tax;
+    service;
+    discount;
+    other;
+    total;
+    state = {
+        result: ""
     }
+    onPressCalculate = () => {
+        let res = (
+            this.parseStringtoNumber(this.tax) + this.parseStringtoNumber(this.service) - this.parseStringtoNumber(this.discount) + this.parseStringtoNumber(this.other)
+        ) / this.parseStringtoNumber(this.total);
+        this.setState({
+            result: `${this.parseStringtoNumber(res)}`,
+        });
+    }
+    
+    parseStringtoNumber(str){
+        return isNaN(Number(str))?0.0:Number(str);
+    }
+
+    updateStatus = (key, value) => {
+        this[key] = value;
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Input label="Tax"></Input>
-                <Input label="Service"></Input>
-                <Input label="Discount"></Input>
-                <Input label="Others"></Input>
-                <Input label="Total"></Input>
+                <Input type='numeric' label="Tax" value={this.tax} updateStatus={this.updateStatus.bind(this, "tax")} name="tax"></Input>
+                <Input type='numeric' label="Service" value={this.sservice} updateStatus={this.updateStatus.bind(this, "service")} name="service"></Input>
+                <Input type='numeric' label="Discount" value={this.discount} updateStatus={this.updateStatus.bind(this, "discount")} name="discount"></Input>
+                <Input type='numeric' label="Others" value={this.other} updateStatus={this.updateStatus.bind(this, "other")} name="other"></Input>
+                <Input type='numeric' label="Total" value={this.total} updateStatus={this.updateStatus.bind(this, "total")} name="total"></Input>
 
-                <TouchableOpacity style={styles.btn}>
-                    <Text style={{color:"white"}}>Calculate</Text>
+                <TouchableOpacity onPress={this.onPressCalculate} style={styles.btn}>
+                    <Text style={{ color: "white" }}>Calculate</Text>
                 </TouchableOpacity>
+
+                <Text>{this.state.result}</Text>
             </View>
         );
     }
@@ -32,6 +56,6 @@ const styles = StyleSheet.create({
         backgroundColor: "#008CBA",
         alignItems: 'center',
         padding: 10,
-        width:"50%"
+        width: "50%"
     }
 });
